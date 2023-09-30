49fbf8c90fbf8a408bc7b3b28d7f27315383ccaf
Mike Borthwick <mhborthwick@gmail.com>
2023-09-30 00:06:24 -0700

    add exit flow

--- src/main.py
+++ src/main.py
@@ -1,20 +1,23 @@
 from enum import Enum
 from typing_extensions import Annotated
 import typer
-from data import get_message
+from data import get_random_message
 
 
 class YesOrNo(str, Enum):
-    yes = "yes"
-    no = "no"
+    yes = "y"  # update to "yes"
+    no = "n"  # update to "no"
 
 
 def main(
         send_message: Annotated[YesOrNo, typer.Option(
             prompt="Do you want to receive a wholesome message?")]
 ):
-    if (send_message == "yes"):
-        message = get_message(1)
+    if (send_message == "n"):
+        print("Ok, no wholesome message for you. Bye!")
+        raise typer.Exit()
+    else:
+        message = get_random_message()
         print(message)
 
 
