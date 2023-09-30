1db7b335df1aff425d74bdc275c84d3e869c4a4e
Mike Borthwick <mhborthwick@gmail.com>
2023-09-29 22:54:02 -0700

    add prompt

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
+from enum import Enum
+from typing_extensions import Annotated
 import typer
 from data import get_message
 
 
-def main():
-    message = get_message(0)
-    print(message)
+class YesOrNo(str, Enum):
+    yes = "yes"
+    no = "no"
+
+
+def main(
+        send_message: Annotated[YesOrNo, typer.Option(
+            prompt="Do you want to receive a wholesome message?")]
+):
+    if (send_message == "yes"):
+        message = get_message(1)
+        print(message)
 
 
 if __name__ == "__main__":
