b1d6d34983ee3d7987b85d20dd938e010e1abfca
Mike Borthwick <mhborthwick@gmail.com>
2023-10-01 23:21:07 -0700

    move messages to json file

--- src/data/messages.py
+++ src/data/messages.py
@@ -1,15 +1,35 @@
+import json
+import os
 import random
+
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class MessageItem:
+    message: str
+    source: str
+
+
+@dataclass
+class MessageResponse:
+    results: List[MessageItem]
 
 
 def get_rand_index(len: int) -> int:
     return random.randint(0, len - 1)
 
 
+def read_messages(file_path: str) -> MessageResponse:
+    with open(file_path, "r") as file:
+        messages = json.load(file)
+    return messages
+
+
 def get_random_message() -> str:
-    messages = [
-        "hello there",
-        "whats up",
-        "how're you doing",
-    ]
-    index = get_rand_index(len(messages))
-    return messages[index]
+    current_dir = os.path.dirname(__file__)
+    file_path = os.path.join(current_dir, "messages.json")
+    messages = read_messages(file_path)
+    index = get_rand_index(len(messages["results"]))
+    return messages["results"][index]
